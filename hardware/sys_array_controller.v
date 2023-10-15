316806ea6b05875fd03ba9263068f2891a8d3682
Vasanth Kumar <vakumar@berkeley.edu>
2023-10-15 16:31:41 -0700

    Add A and D memory (addr) and sys array (data + valid) management to array controller

--- hardware/sys_array_controller.v
+++ hardware/sys_array_controller.v
@@ -1,7 +1,6 @@
 module sys_array_controller
     #(
-        parameter BITWIDTH, ADDRWIDTH,
-            MESHROWS, MESHCOLS, BITWIDTH, TILEROWS, TILECOLS
+        parameter BITWIDTH, ADDRWIDTH, MESHUNITS, TILEUNITS
     )
     (
         input clock,
@@ -21,17 +20,17 @@ module sys_array_controller
         output load_finished,
 
         // MEMORY READ SIGNALS
-        input [BITWIDTH-1:0] A [MESHROWS-1:0][TILEROWS-1:0],
-        input [BITWIDTH-1:0] D [MESHCOLS-1:0][TILECOLS-1:0],
-        input [BITWIDTH-1:0] B [MESHCOLS-1:0][TILECOLS-1:0],
-        output [ADDRWIDTH-1:0] A_row_read_addrs [MESHROWS-1:0],
-        output [ADDRWIDTH-1:0] D_col_read_addrs [MESHCOLS-1:0],
-        output [ADDRWIDTH-1:0] B_col_read_addrs [MESHCOLS-1:0],
+        input signed [BITWIDTH-1:0] A [MESHUNITS-1:0][TILEUNITS-1:0],
+        input signed [BITWIDTH-1:0] D [MESHUNITS-1:0][TILEUNITS-1:0],
+        input signed [BITWIDTH-1:0] B [MESHUNITS-1:0][TILEUNITS-1:0],
+        output [ADDRWIDTH-1:0] A_row_read_addrs [MESHUNITS-1:0],
+        output [ADDRWIDTH-1:0] D_col_read_addrs [MESHUNITS-1:0],
+        output [ADDRWIDTH-1:0] B_col_read_addrs [MESHUNITS-1:0],
 
         // MEMORY WRITE SIGNALS
-        output [BITWIDTH-1:0] C [MESHCOLS-1:0][TILECOLS-1:0],
-        output [ADDRWIDTH-1:0] C_col_write_addrs [MESHCOLS-1:0],
-        output C_write_valid [MESHCOLS-1]
+        output [BITWIDTH-1:0] C [MESHUNITS-1:0][TILEUNITS-1:0],
+        output [ADDRWIDTH-1:0] C_col_write_addrs [MESHUNITS-1:0],
+        output C_write_valid [MESHUNITS-1]
     );
 
     localparam 
@@ -40,23 +39,86 @@ module sys_array_controller
         LOCK_ONE = 2'b10;
 
     reg [1:0] comp_lock;
+    reg [31:0] comp_tick_ctr;
+    reg comp_complete;
     reg [ADDRWIDTH-1:0] A_base_addr;
     reg [ADDRWIDTH-1:0] D_base_addr;
     reg [ADDRWIDTH-1:0] C_base_addr;
+    wire A_valid [MESHUNITS=1:0][TILEUNITS-1:0];
+    wire D_valid [MESHUNITS=1:0][TILEUNITS-1:0];
 
     reg [1:0] load_lock;
+    reg [31:0] load_tick_ctr;
+    reg load_complete;
     reg [ADDRWIDTH-1:0] B_base_addr;
+
+    always @(*) begin
+        if (comp_lock == LOCK_FREE) begin
+            A_valid = 0;
+            D_valid = 0;
+            A_row_read_addrs = 0;
+            D_row_read_addrs = 0;
+        end
+        else begin
+            // READ ADDRS
+            genvar i, j;
+            for (i = 0; i < MESHUNITS; i++) begin
+                if (comp_tick_ctr >= i && comp_tick_ctr < (MESHUNITS * TILEUNITS) + i) begin
+                    A_row_read_addrs[i] = A_base_addr + (comp_tick_ctr - i) * MESHUNITS * TILEUNITS + i * TILEUNITS;
+                    D_row_read_addrs[i] = D_base_addr + (comp_tick_ctr - i) * MESHUNITS * TILEUNITS + i * TILEUNITS;
+                end
+                else begin
+                    A_row_read_addrs[i] = 0;
+                    D_row_read_addrs[i] = 0;
+                end
+            end
+
+            // READ VALID (TO SYS ARRAY)
+            if (comp_tick_ctr == 0) begin
+                A_valid = 0;
+                D_valid = 0;
+            end
+            else begin
+                wire [31:0] prev_comp_tick_ctr = comp_tick_ctr - 1;
+                for (i = 0; i < MESHUNITS; i++) begin
+                    for (j = 0; j < TILEUNITS; j++) begin
+                        if (prev_comp_tick_ctr >= i && prev_comp_tick_ctr < (MESHUNITS * TILEUNITS) + i) begin
+                            A_valid[i][j] = 1;
+                            D_valid[i][j] = 1;
+                        end
+                        else begin
+                            A_valid[i][j] = 0;
+                            D_valid[i][j] = 0;
+                        end
+                    end
+                end
+            end
+        end
+    end
 
     always @(posedge clock) begin
         if (reset) begin
             comp_lock <= LOCK_FREE;
+            comp_complete <= 0;
             load_lock <= LOCK_FREE;
+            load_complete <= 0;
         end
         else begin
+
+            // SYNCHRONIZATION UPDATE
+            if (comp_complete) begin
+                comp_complete <= 0;
+                comp_lock <= LOCK_FREE;
+            end
+            if (load_complete) begin
+                load_complete <= 0;
+                load_lock <= LOCK_FREE;
+            end
             if (comp_lock == LOCK_FREE && load_lock == LOCK_FREE) begin
                 if (comp_lock_req[0]) begin
                     // assign comp_lock to 0 and try to assign load_lock to 1
                     comp_lock <= LOCK_ZERO;
+                    comp_tick_ctr <= 0;
                     A_base_addr <= A_addr[0];
                     D_base_addr <= D_addr[0];
                     C_base_addr <= C_addr[0];
@@ -71,6 +133,7 @@ module sys_array_controller
                 else if (comp_lock_req[1]) begin
                     // assign comp_lock to 1 and try to assign load lock to 0
                     comp_lock <= LOCK_ONE;
+                    comp_tick_ctr <= 0;
                     A_base_addr <= A_addr[1];
                     D_base_addr <= D_addr[1];
                     C_base_addr <= C_addr[1];
@@ -102,12 +165,14 @@ module sys_array_controller
                 // try to assign comp lock to first thread that doesn't hold load lock
                 if (load_lock != LOCK_ZERO && comp_lock_req[0]) begin
                     comp_lock <= LOCK_ZERO;
+                    comp_tick_ctr <= 0;
                     A_base_addr <= A_addr[0];
                     D_base_addr <= D_addr[0];
                     C_base_addr <= C_addr[0];
                 end
                 else if (load_lock != LOCK_ONE && comp_lock_req[1]) begin
                     comp_lock <= LOCK_ONE;
+                    comp_tick_ctr <= 0;
                     A_base_addr <= A_addr[1];
                     D_base_addr <= D_addr[1];
                     C_base_addr <= C_addr[1];
@@ -128,19 +193,19 @@ module sys_array_controller
     end
 
 
-    sys_array #(parameter MESHROWS=MESHROWS, MESHCOLS=MESHCOLS, BITWIDTH=BITWIDTH, TILEROWS=TILEROWS, TILECOLS=TILECOLS)
+    sys_array #(parameter MESHROWS=MESHUNITS, MESHCOLS=MESHUNITS, BITWIDTH=BITWIDTH, TILEROWS=TILEUNITS, TILECOLS=TILEUNITS)
     sys_array_module (
         .clock(clock),
         .reset(reset),
         .in_dataflow(),
-        .in_a(),
-        .in_a_valid(),
+        .in_a(A),
+        .in_a_valid(A_valid),
         .in_b(),
-        .in_d(),
+        .in_d(D),
         .in_propagate(),
         .in_b_shelf_life(),
         .in_b_valid(),
-        .in_d_valid(),
+        .in_d_valid(D_valid),
         .out_c(),
         .out_c_valid()
     );
